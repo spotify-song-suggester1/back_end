02426641b23f3387aec651f464c1393248503685
Christian <christiancorby@gmail.com>
2020-07-29 18:18:10 -0400

    port80

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require("./server");
 
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 80;
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
